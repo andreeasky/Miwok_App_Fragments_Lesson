ea4608d4a214d8e94a1231a40059aa3535ce2243
Andreea Manea <andreeasky@yahoo.com>
2017-06-05 07:11:51 +0300

    Initial commit

---
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -0,0 +1,97 @@
+package com.example.android.miwok;
+
+import android.app.Activity;
+import android.support.v4.content.ContextCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by User on 5/21/2017.
+ */
+
+public class WordAdapter extends ArrayAdapter<Word> {
+
+    public int colorResourceId;
+
+    public int color;
+
+    /**
+     * //This is our own custom constructor (it doesn't mirror a superclass constructor).
+     * The context is used to inflate the layout file, and the list is the data we want
+     * to populate into the lists.
+     *
+     * @param context The current context. Used to inflate the layout file.
+     * @param words A List of words objects to display in a list.
+     * @param colorResourceId The color for each category.
+     */
+    public WordAdapter(Activity context, ArrayList<Word> words, int colorResourceId) {
+        // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
+        // the second argument is used when the ArrayAdapter is populating a single TextView.
+        // Because this is a custom adapter for two TextViews, the adapter is not
+        // going to use this second argument, so it can be any value. Here, we used 0.
+        super(context, 0, words);
+
+        color = ContextCompat.getColor(context, colorResourceId);
+    }
+
+    @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            // Check if the existing view is being reused, otherwise inflate the view
+            View listItemView = convertView;
+            if (listItemView == null) {
+                listItemView = LayoutInflater.from(getContext()).inflate(
+                        R.layout.list_item, parent, false);
+
+                LinearLayout textContainer = (LinearLayout) listItemView.findViewById(R.id.text_container);
+
+                textContainer.setBackgroundColor(color);
+
+                return textContainer;
+            }
+
+        // Get the {@link Word} object located at this position in the list
+        Word currentWord = getItem(position);
+
+        // Find the TextView in the list_item.xml layout with the ID version_name
+        TextView nameTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
+        // Get the version name from the current Word object and
+        // set this text on the name TextView
+        nameTextView.setText(currentWord.getMiwokTranslation());
+
+        // Find the TextView in the list_item.xml layout with the ID version_number
+        TextView numberTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
+
+        // Get the version number from the current Word object and
+        // set this text on the number TextView
+        numberTextView.setText(currentWord.getDefaultTranslation());
+
+        // Find the ImageView in the list_item.xml layout with the ID image
+        ImageView imageView = (ImageView) listItemView.findViewById(R.id.image);
+
+        // Check if an image is provided for this word or not
+            if (currentWord.hasImage()) {
+                // If an image is available, display the provided image based on the resource ID
+                        imageView.setImageResource(currentWord.getImageResourceId());
+                // Make sure the view is visible
+                        imageView.setVisibility(View.VISIBLE);
+            } else {
+                // Otherwise hide the ImageView (set visibility to GONE)
+                        imageView.setVisibility(View.GONE);
+            }
+
+        // Return the whole list item layout (containing 2 TextViews and an ImageView)
+        // so that it can be shown in the ListView
+        return listItemView;
+    }
+}
+
+
+

b2c9a0720fb736cd834e3b55d791dab503d150cc
Andreea Manea <andreeasky@yahoo.com>
2017-06-10 23:12:02 +0300

    Update to the PhrasesActivity - Java file

--- app/src/main/java/com/example/android/miwok/PhrasesActivity.java
+++ app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -19,16 +19,16 @@ public class PhrasesActivity extends AppCompatActivity {
 
         // Create a list of words
         ArrayList<Word> words = new ArrayList<Word>();
-        words.add(new Word("Where are you going?", "minto wuksus", R.drawable.number_one, R.raw.phrase_where_are_you_going));
-        words.add(new Word("What is your name?", "tinnә oyaase'nә", R.drawable.number_one, R.raw.phrase_what_is_your_name));
-        words.add(new Word("My name is...", "oyaaset...", R.drawable.number_one, R.raw.phrase_my_name_is));
-        words.add(new Word("How are you feeling?", "michәksәs?", R.drawable.number_one, R.raw.phrase_how_are_you_feeling));
-        words.add(new Word("I’m feeling good.", "kuchi achit", R.drawable.number_one, R.raw.phrase_im_feeling_good));
-        words.add(new Word("Are you coming?", "әәnәs'aa?", R.drawable.number_one, R.raw.phrase_are_you_coming));
-        words.add(new Word("Yes, I’m coming.", "hәә’ әәnәm", R.drawable.number_one, R.raw.phrase_yes_im_coming));
-        words.add(new Word("I’m coming.", "әәnәm", R.drawable.number_one, R.raw.phrase_im_coming));
-        words.add(new Word("Let’s go.", "yoowutis", R.drawable.number_one, R.raw.phrase_lets_go));
-        words.add(new Word("Come here.", "әnni'nem", R.drawable.number_one, R.raw.phrase_come_here));
+        words.add(new Word("Where are you going?", "minto wuksus", R.raw.phrase_where_are_you_going));
+        words.add(new Word("What is your name?", "tinnә oyaase'nә", R.raw.phrase_what_is_your_name));
+        words.add(new Word("My name is...", "oyaaset...", R.raw.phrase_my_name_is));
+        words.add(new Word("How are you feeling?", "michәksәs?", R.raw.phrase_how_are_you_feeling));
+        words.add(new Word("I’m feeling good.", "kuchi achit", R.raw.phrase_im_feeling_good));
+        words.add(new Word("Are you coming?", "әәnәs'aa?", R.raw.phrase_are_you_coming));
+        words.add(new Word("Yes, I’m coming.", "hәә’ әәnәm", R.raw.phrase_yes_im_coming));
+        words.add(new Word("I’m coming.", "әәnәm", R.raw.phrase_im_coming));
+        words.add(new Word("Let’s go.", "yoowutis", R.raw.phrase_lets_go));
+        words.add(new Word("Come here.", "әnni'nem", R.raw.phrase_come_here));
 
         // Create an {@link WordAdapter}, whose data source is a list of {@link Word}s. The
         // adapter knows how to create list items for each item in the list.
